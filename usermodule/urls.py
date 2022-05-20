841f9c90164489dfd9a325a9a33b3ca952b769fe
Sakib Rahman <srskb15@gmail.com>
2022-05-20 13:48:48 +0600

    SA | First Release

--- usermodule/urls.py
+++ usermodule/urls.py
@@ -7,7 +7,6 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
-router.register(r'users/<string:pk>', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
--- usermodule/views.py
+++ usermodule/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from usermodule.models import User
 from usermodule.serializers import UserSerializer
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 # Create your views here.
 
@@ -14,12 +16,42 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def get_queryset(self):
-        queryset = User.objects.all()
-        username = self.request.query_params.get('username', None)
-        if username is not None:
-            queryset = queryset.filter(username=username)
-        return queryset
+    def get_object(self):
+        pk = self.kwargs['pk']
+        address = User.objects.get(address=pk)
+        return UserSerializer(address).data
     
+    @action(methods=['POST'],detail = False)
+    def create_user(self, request):
+        address = request.data['address']
+        hash = request.data['hash']
+        user = User.objects.create(address=address, hash=hash)
+        return Response(UserSerializer(user).data)
 
+    @action(methods=['PUT'],detail = False)
+    def update_user(self, request):
+        address = request.data['address']
+        hash = request.data['hash']
+        user = User.objects.get(address=address)
+        user_serailizer = UserSerializer(user, data=request.data)
+        if user_serailizer.is_valid():
+            user_serailizer.save()
+            return Response(user_serailizer.data)
+        else:
+            return Response(user_serailizer.errors)
 
+    @action(methods=['DELETE'], detail=False)
+    def delete_user(self, request):
+        address = request.data['address']
+        user = User.objects.get(address=address).delete()
+        return Response(UserSerializer(user).data)
+
+    @action(methods=['file'],detail = False)
+    def import_csv(self, request):
+        file = request.data['file']
+        with open(file, 'r') as f:
+            for line in f:
+                address, hash = line.split(',')
+                if not User.objects.get(address=address).exists():
+                    user = User.objects.create(address=address, hash=hash)
+        return Response(UserSerializer(user).data)
