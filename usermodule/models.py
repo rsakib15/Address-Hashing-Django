9e2938b1a8802d1302c0df752fa89b4972d0b45c
Sakib Rahman <srskb15@gmail.com>
2022-05-20 10:45:41 +0600

    SA | Model updated

--- usermodule/models.py
+++ usermodule/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
+class User(models.Model):
     address = models.CharField(max_length=255, blank=False)
     hash = models.TextField(blank=False)
     
