69ade6ebc7b8af34770849130257c03106b79e67
Sakib Rahman <srskb15@gmail.com>
2022-05-20 16:20:16 +0600

    SA | asgi updated

--- addresshashing/asgi.py
+++ addresshashing/asgi.py
@@ -1,5 +1,6 @@
 """
-ASGI config for adresshashing project.
+ASGI config for addresshashing project.
+ASGI config for addresshashing project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +12,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adresshashing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'addresshashing.settings')
 
 application = get_asgi_application()
